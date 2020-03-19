07d0be4f30b1fef9965b8ff5187cab3d8ad9b35d
Justine Tarlet <justinetarlet@gmail.com>
2020-03-19 17:23:52 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('email-form');
+const email = document.getElementById('email');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const emailVal = email.value;
+
+    if (validateEmail(emailVal)) {
+        form.classList.remove('error');
+        form.classList.add('success');
+        document.body.innerHTML='Sucess! See you soon!'
+    } else {
+        form.classList.add('error');
+        form.classList.remove('success');
+
+       
+    }
+});
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
